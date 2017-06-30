65b44a33840f97ee26f6a9e0616508416673fb87
Greg Lazarev <greg@thoughtbot.com>
2017-06-30 15:09:07 -0700

    handles only subscription deleted events

--- app/controllers/deleted_subscriptions_controller.rb
+++ app/controllers/deleted_subscriptions_controller.rb
@@ -2,9 +2,11 @@ class DeletedSubscriptionsController < ApplicationController
   skip_before_action :authenticate, only: :create
 
   def create
-    subscription_id = params.dig("data", "object", "id")
-    Subscription.where(stripe_subscription_id: subscription_id).destroy_all.each do |sub|
-      sub.repo.deactivate
+    if params.dig("type") == "customer.subscription.deleted"
+      subscription_id = params.dig("data", "object", "id")
+      Subscription.where(stripe_subscription_id: subscription_id).destroy_all.each do |subscription|
+        subscription.repo.deactivate
+      end
     end
   end
 end
--- spec/requests/subscriptions_spec.rb
+++ spec/requests/subscriptions_spec.rb
@@ -1,35 +1,54 @@
 require "rails_helper"
 
 RSpec.describe "Subscriptions" do
-  it "deletes a subscription" do 
-    subscription = create(:subscription, :active)
-    another_subscription = create(
-      :subscription,
-      :active,
-      stripe_subscription_id: subscription.stripe_subscription_id,
-    )
-    params = {
-      "type": "customer.subscription.deleted",
-      "object": "event",
-      "pending_webhooks": 1,
-      "data": {
-        "object": {
-          "id": subscription.stripe_subscription_id,
-          "object": "subscription",
-          "customer": "cus_00000000000000",
-          "quantity": 1,
-          "status": "canceled",
+  context "when event is for subscription deleted" do
+    it "deletes all subscription records and deactivates their repos" do 
+      subscription = create(:subscription, :active)
+      another_subscription = create(
+        :subscription,
+        :active,
+        stripe_subscription_id: subscription.stripe_subscription_id,
+      )
+      params = {
+        "type": "customer.subscription.deleted",
+        "object": "event",
+        "data": {
+          "object": {
+            "id": subscription.stripe_subscription_id,
+            "object": "subscription",
+          }
         }
       }
-    }
-    
-    post "/deleted_subscriptions", params: params
+      
+      post "/deleted_subscriptions", params: params
 
-    subscription.reload
-    another_subscription.reload
-    expect(subscription).to be_deleted
-    expect(another_subscription).to be_deleted
-    expect(subscription.repo).not_to be_active
-    expect(another_subscription.repo).not_to be_active
+      subscription.reload
+      another_subscription.reload
+      expect(subscription).to be_deleted
+      expect(another_subscription).to be_deleted
+      expect(subscription.repo).not_to be_active
+      expect(another_subscription.repo).not_to be_active
+    end
+  end
+
+  context "when event is not subscription deleted" do
+    it "does not delete subscription" do
+      subscription = create(:subscription, :active)
+      params = {
+        "type": "customer.subscription.updated",
+        "data": {
+          "object": {
+            "id": subscription.stripe_subscription_id,
+            "object": "subscription",
+          }
+        }
+      }
+
+      post "/deleted_subscriptions", params: params
+
+      subscription.reload
+      expect(subscription).not_to be_deleted
+      expect(subscription.repo).to be_active
+    end
   end
 end
