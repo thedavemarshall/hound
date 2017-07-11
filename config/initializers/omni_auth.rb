a98ac14982036737aec9d69a5f55dcb94cf21b4d
David Marshall <dmarshall@groupon.com>
2017-07-11 15:52:52 -0700

    Update some gems and add groupondev GHE

--- config/initializers/omni_auth.rb
+++ config/initializers/omni_auth.rb
@@ -1,5 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
+# FOR LOCAL TESTING ONLY
+# OmniAuth.config.add_mock(:github, {:uid => '12345'})
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   setup = ->(env) do
     options = GithubAuthOptions.new(env)
@@ -10,6 +13,13 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :github,
     ENV['GITHUB_CLIENT_ID'],
     ENV['GITHUB_CLIENT_SECRET'],
-    setup: setup,
+    {
+      setup: setup,
+      client_options: {
+        site: 'https://github.groupondev.com/api/v3',
+        authorize_url: 'https://github.groupondev.com/login/oauth/authorize',
+        token_url: 'https://github.com/groupondev.com/login/oauth/access_token'
+      }
+    }
   )
 end
